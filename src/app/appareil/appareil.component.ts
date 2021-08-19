0369f2b1fcbb073dd50d8432a0bb634460119496
vgo <vgo@ccabe.com>
2021-08-19 10:11:16 +0200

    2-Gérez des données dynamiques String interpolation

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  appareilName: string = 'Machine à laver';
+  appareilStatus: string = 'éteint';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+}
