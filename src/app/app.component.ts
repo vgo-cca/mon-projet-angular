790ce3cdbf8c60ba70151028fc11c5915a45fe2c
vgo <vgo@ccabe.com>
2021-08-20 16:06:56 +0200

    4-Utilisez et paramétrez les Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mon-projet-angular';
   isAuth = false;
+  lastUpdate = new Date();
 
   constructor() {
     setTimeout(
