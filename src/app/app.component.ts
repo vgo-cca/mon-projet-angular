8dd93583875ba95607a6050337e1c0e8a7237da1
vgo <vgo@ccabe.com>
2021-09-03 16:40:25 +0200

    7 RxJS: Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 import { AppareilService } from './services/appareil.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { AppareilService } from './services/appareil.service';
 export class AppComponent implements OnInit {
   title = 'mon-projet-angular';
   isAuth = false;
+  
+  secondes!: number;
 
   constructor( public appareilService: AppareilService) {
     setTimeout(
@@ -18,6 +21,27 @@ export class AppComponent implements OnInit {
     );
   }
 
-  ngOnInit(){}
+  ngOnInit(
+  ){
+    // l'observable
+    // interval() crée un Observable qui émet un chiffre croissant à intervalles réguliers et
+    // prend le nombre de millisecondes souhaité pour l'intervalle comme argument.
+    let counter = interval(1000);
+
+    // l'observer ( subscribe() )
+    // L'Observable émet trois types d'informations : des données, une erreur, ou un message complete. 
+    counter.subscribe(
+      (value) =>{
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('il y a une erreur');
+        console.log(error);
+      },
+      () => {
+        console.log('observer complete');
+      }
+    );
+  }
 
 }
