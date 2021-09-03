fbf348d0f3cbd8f8dcf2a2a90fa3c7c5845c3e95
vgo <vgo@ccabe.com>
2021-09-03 16:59:24 +0200

    RxJs: Subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 import { AppareilService } from './services/appareil.service';
 
 @Component({
@@ -7,11 +7,12 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'mon-projet-angular';
   isAuth = false;
   
   secondes!: number;
+  counterSubscription!: Subscription;
 
   constructor( public appareilService: AppareilService) {
     setTimeout(
@@ -21,16 +22,17 @@ export class AppComponent implements OnInit {
     );
   }
 
-  ngOnInit(
-  ){
+  ngOnInit(){
     // l'observable
     // interval() crée un Observable qui émet un chiffre croissant à intervalles réguliers et
     // prend le nombre de millisecondes souhaité pour l'intervalle comme argument.
     let counter = interval(1000);
 
     // l'observer ( subscribe() )
-    // L'Observable émet trois types d'informations : des données, une erreur, ou un message complete. 
-    counter.subscribe(
+    // L'Observable émet trois types d'informations : des données, une erreur, ou un message complete.
+    // Afin d'éviter tout problème, quand vous utilisez des Observables personnalisés,
+    // il est vivement conseillé de stocker la souscription dans un objet Subscription
+    this.counterSubscription = counter.subscribe(
       (value) =>{
         this.secondes = value;
       },
@@ -42,6 +44,13 @@ export class AppComponent implements OnInit {
         console.log('observer complete');
       }
     );
+  }
+
+  ngOnDestroy() {
+    //  La fonction  unsubscribe()  détruit la souscription et
+    // empêche les comportements inattendus liés aux Observables infinis,
+    // donc n'oubliez pas de unsubscribe !
+    this.counterSubscription.unsubscribe();
   }
 
 }
